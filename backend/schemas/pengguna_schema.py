4f2d117a0d8e247787f37cb002dee3b8c5e506ce
LyonAD <lyon.ambrosio.djuanda@gmail.com>
2025-06-16 12:16:09 +0700

    Retrying with other methods

--- backend/schemas/pengguna_schema.py
+++
@@ -1,8 +0,0 @@
-import graphene
-from backend.resolvers.pengguna_resolver import RegisterPengguna, LoginPengguna
-
-class Mutation(graphene.ObjectType):
-    register_pengguna = RegisterPengguna.Field()
-    login_pengguna = LoginPengguna.Field()
-
-schema = graphene.Schema(mutation=Mutation)
--- backend/server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_graphql import GraphQLView
-from backend.schemas.pengguna_schema import schema
-
-app = Flask(__name__)
-app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
-
-if __name__ == '__main__':
-    app.run(debug=True)
